18816ab3742a6d9451ae30530af846c0cc08c027
midhuna-06 <midhuna.a2023cse@sece.ac.in>
2026-03-21 11:18:53 +0530

    Added post api to add memory

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Memory = require("./models/Memory");
 require("dotenv").config();
 
 const app = express();
+
 
 app.use(express.json());
 
@@ -18,4 +20,16 @@ app.get("/", (req, res) => {
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+
+//post api
+
+app.post("/add-memory", async (req, res) => {
+  try {
+    const newMemory = new Memory(req.body);
+    const savedMemory = await newMemory.save();
+    res.json(savedMemory);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
