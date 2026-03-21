4f58f1488110e101172eefcf2bb1c0b9af36f6b0
midhuna-06 <midhuna.a2023cse@sece.ac.in>
2026-03-21 11:30:38 +0530

    Added user schema and register API with JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Memory = require("./models/Memory");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
+
 require("dotenv").config();
 
 const app = express();
@@ -29,6 +33,44 @@ app.post("/add-memory", async (req, res) => {
     const newMemory = new Memory(req.body);
     const savedMemory = await newMemory.save();
     res.json(savedMemory);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, dob, email, password, confirmPassword } = req.body;
+
+    // check password match
+    if (password !== confirmPassword) {
+      return res.status(400).json({ msg: "Passwords do not match" });
+    }
+
+    // check user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // save user
+    const user = new User({
+      name,
+      dob,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    // create token
+    const token = jwt.sign({ id: user._id }, "secretkey");
+
+    res.json({ token });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
